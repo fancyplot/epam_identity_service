80a6c62e97c3993a63543bb4299e126e58bdb8c9
DzianisKhmialniukTR <Dzianis.Khmialniuk@thomsonreuters.com>
2023-12-03 17:37:59 +0300

    Add project files.

---
+++ DuendeIdentityService/Config.cs
@@ -0,0 +1,163 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using IdentityModel;
+
+namespace DuendeIdentityService
+{
+    public static class Config
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "818727",
+                        Username = "alice",
+                        Password = "alice",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.Role, "admin"),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address),
+                                IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "88421113",
+                        Username = "bob",
+                        Password = "bob",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Bob"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.Role, "user"),
+                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address),
+                                IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    }
+                };
+            }
+        }
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+            new ApiScope("catalog.read"),
+            new ApiScope("catalog.create"),
+            new ApiScope("catalog.update"),
+            new ApiScope("catalog.delete"),
+            new ApiScope("carting.all")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources => new[]
+        {
+            new ApiResource("catalog")
+            {
+                Scopes = new List<string> { "catalog.read", "catalog.create", "catalog.update", "catalog.delete"},
+                ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())},
+                UserClaims = new List<string> {"role"}
+            },
+            new ApiResource("carting")
+            {
+                Scopes = new List<string> { "carting.all" },
+                ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())},
+                UserClaims = new List<string> {"role"}
+            }
+        };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                //// m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "Manager",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "catalog.read", "catalog.create", "catalog.update", "catalog.delete", "carting.all" }
+                },
+
+                new Client
+                {
+                    ClientId = "Buyer",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedScopes = { "catalog.read", "carting.all" }
+                },
+
+            // interactive client using code flow + pkce
+            //new Client
+            //{
+            //    ClientId = "Buyer",
+            //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+            //    AllowedGrantTypes = GrantTypes.Code,
+
+            //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+            //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+            //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+            //    AllowOfflineAccess = true,
+            //    AllowedScopes = { "openid", "profile", "catalog.read" }
+            //},
+
+            //// interactive client using code flow + pkce
+            //new Client
+            //{
+            //    ClientId = "Manager",
+            //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+            //    AllowedGrantTypes = GrantTypes.Code,
+
+            //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+            //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+            //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+            //    AllowOfflineAccess = true,
+            //    AllowedScopes = { "openid", "profile", "catalog.read", "catalog.create", "catalog.update", "catalog.delete" }
+            //},
+            };
+    }
+}
---
+++ DuendeIdentityService/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace DuendeIdentityService.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
